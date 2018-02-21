c741bda7059484f44e0b654864c8f6df70e14cc2
raveek <ravindrakenchappa@gmail.com>
2018-02-21 11:56:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
     title: 'Article One | Ravi K',
     heading : 'Artical One',
     date : '8 Feb ,2018',
