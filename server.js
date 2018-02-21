573867825d8f87397b689274311ff9593885874b
raveek <ravindrakenchappa@gmail.com>
2018-02-21 13:45:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
