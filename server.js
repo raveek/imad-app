56dd73ed1493b62d9ccf9fcdfa6bf481e9df7a98
raveek <ravindrakenchappa@gmail.com>
2018-02-21 14:40:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/counter', function (req, res) {
+    counter = counter +1;
+    res.send(counter.toString());
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
